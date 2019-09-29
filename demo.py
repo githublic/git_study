920f1f25912b671afcc454822e9962f03cf681d8
githublic <875261448@qq.com>
2019-09-29 16:14:42 +0800

    解析百度首页

--- demo.py
+++ demo.py
@@ -6,10 +6,18 @@
 # @File    : demo.py
 # @Software: PyCharm
 import requests
+import parsel
 res = requests.get('http://www.baidu.com')
+res.encoding=res.apparent_encoding
 print(res.status_code)
 if res.status_code==200:
     print('连接成功')
+    sel = parsel.Selector(res.text)
+    wb = sel.xpath('//a/text()').getall()
+    print(wb)
 else:
     print('网络连接错误')
+
+
+
 # print('hello yuan')
